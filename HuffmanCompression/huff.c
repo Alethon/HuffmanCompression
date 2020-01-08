799970d588625c35026478c29004e4dcfaddf331
unknown <jacob.laster23@gmail.com>
2020-01-08 15:36:36 -0500

    changing to clearer method definition structure

--- HuffmanCompression/huff.c
+++ HuffmanCompression/huff.c
@@ -4,26 +4,11 @@
 #include <stdbool.h>
 #include <stdint.h>
 
+#include "huffTree.h"
+
 #include "huff.h"
+#include "unhuff.h"
 
-#define UNIQUE_BYTES 256
-
-//Tree methods
-Tree* TreeConstructor(void);
-void sortTreesDescending(Tree**, int);
-int getTreeCount(Tree**);
-Tree* branchFromTrees(Tree*, Tree*);
-void makeTreeFromSorted(Tree**);
-void exploreTree(Tree*, int, int**, int);
-int** makeEncodingTable(Tree**);
-
-//initialization methods
-Tree** initializeUnicodeFrequencies(void);
-
-//data processing
-void getUnicodeFrequencies(Tree**, long long*, FILE*);
-void huff(char*, char*);
-void unhuff(char*, char*);
 
 //testing
 void printTree(Tree*);
@@ -40,24 +25,6 @@ int main() {
 	return 0;
 }
 
-//allocates and initializes a Tree (branch), returning its pointer
-Tree* TreeConstructor() {
-	Tree* tree = (Tree*)malloc(sizeof(Tree));
-	tree->left = NULL;
-	tree->right = NULL;
-	tree->n = 255;
-	tree->f = 0;
-	return tree;
-}
-
-void TreeDestructor(Tree* t) {
-	if (t == NULL)
-		return;
-	TreeDestructor(t->left);
-	TreeDestructor(t->right);
-	free(t);
-	return;
-}
 
 //returns a pointer to a UNIQUE_BYTES-length pointer to future Tree structures, which hold the frequency of each unicode character
 Tree** initializeUnicodeFrequencies() {
@@ -81,73 +48,6 @@ void getUnicodeFrequencies(Tree** uni, long long* counts, FILE* fp) {
 	for (int i = 0; i < (int)UNIQUE_BYTES; i++)
 		counts[i] = uni[i]->f;
 	return;
-}
-
-//bubble sorting here, since the pointer has relatively few elements.
-//Given a much larger, sparse pointer, zero freqency elements should be removed first.
-void sortTreesDescending(Tree** uni, int n) {
-	Tree* temp;
-	for (int i = n - 1 ; i > 0; i--) {
-		for (int j = 0; j < i; j++) {
-			if ((uni[j])->f < (uni[j + 1])->f) {
-				temp = uni[j];
-				uni[j] = uni[j + 1];
-				uni[j + 1] = temp;
-			}
-		}
-	}
-	return;
-}
-
-int getTreeCount(Tree** uni) {
-	int nonzero;
-	for (nonzero = 0; nonzero < (int)UNIQUE_BYTES; nonzero++)
-		if (uni[nonzero]->f == 0)
-			break;
-	return nonzero;
-}
-
-//assume both are non-null trees 
-Tree* branchFromTrees(Tree* t1, Tree* t2) {
-	Tree* branch = TreeConstructor();
-	branch->f = t1->f + t2->f;
-	if (t2->f < t1->f) {
-		branch->left = t2;
-		branch->right = t1;
-	} else {
-		branch->left = t1;
-		branch->right = t2;
-	}
-	return branch;
-}
-
-void makeTreeFromSorted(Tree** uni) {
-	int n = getTreeCount(uni);
-	while (n-- > 1) {
-		uni[n - 1] = branchFromTrees(uni[n - 1], uni[n]);
-		uni[n] = NULL;
-		sortTreesDescending(uni, n);
-	}
-	return;
-}
-
-void exploreTree(Tree* t, int loc, int** table, int n) {
-	if (t->left == NULL) {
-		table[t->n] = (int*)malloc(2 * sizeof(int));
-		table[t->n][0] = n - 1;
-		table[t->n][1] = loc;
-	} else {
-		loc <<= 1;
-		exploreTree(t->left, loc, table, n + 1);
-		exploreTree(t->right, loc + 1, table, n + 1);
-	}
-	return;
-}
-
-int** makeEncodingTable(Tree** uni) {
-	int** table = (int**)calloc((int)UNIQUE_BYTES, sizeof(int*));
-	exploreTree(uni[0], 0, table, 1);
-	return table;
 }
 
 int getPaddingBits(long long* count, int** table) {
@@ -284,155 +184,3 @@ void huff(char* fileIn, char* fileOut) {
 	return;
 }
 
-void readPackingData(FILE* fp, int* paddingBits, int* treeBits) {
-	uint16_t head;
-	fread(&head, 2, 1, fp);
-	*paddingBits = head >> 12;
-	*treeBits = head & 4095;
-	return;
-}
-
-uint8_t* readBitTree(FILE* fp, int treeBits) {
-	int treeBytes = treeBits / 8 + (treeBits % 8 ? 1 : 0);
-	//printf("tb%d\n", treeBytes);
-	uint8_t* tree = (uint8_t*)calloc(treeBytes + 1, sizeof(uint8_t));
-	fread(tree, 1, treeBytes, fp);
-	//for (int i = 0; i < treeBytes; i++)
-	//	printf("%02x", tree[i]);
-	return tree;
-}
-
-//read starting at effective index n
-int readBitsFromTree(uint8_t* bitTree, int n, int num) {
-	int output = 0;
-	int i = n / 8;
-	int mask = (255 >> (8 - num)) << (16 - num);
-	output = (bitTree[i] << 8) + bitTree[i + 1];
-	output = (output & (mask >> (n % 8))) >> (16 - num - n % 8);
-	//printf("read %d\n", output);
-	return output;
-}
-
-Tree* makeTreeFromBits(uint8_t* bitTree, int* n) {
-	Tree* t = TreeConstructor();
-	if (readBitsFromTree(bitTree, *n, 1) == 1) {
-		t->n = readBitsFromTree(bitTree, *n + 1, 8);
-		*n = *n + 9;
-		//printf("%d\n", t->n);
-	} else {
-		//printf("0\n");
-		*n = *n + 1;
-		t->left = makeTreeFromBits(bitTree, n);
-		t->right = makeTreeFromBits(bitTree, n);
-	}
-	return t;
-}
-
-int** makeDecodingTable(int** etable) {
-	return NULL;
-}
-
-void unhuff(char* fileIn, char* fileOut) {
-	//open files
-	FILE* fpi;
-	FILE* fpo;
-	fopen_s(&fpi, fileIn, "r");
-	if (fpi == NULL)
-		return;
-	fopen_s(&fpo, fileOut, "w+");
-	if (fpo == NULL) {
-		fclose(fpi);
-		return;
-	}
-
-	//get the Huffman tree and encoding table 
-	int n = 0;
-	int paddingBits, treeBits;
-	readPackingData(fpi, &paddingBits, &treeBits);
-	uint8_t* bitTree = readBitTree(fpi, treeBits);
-	Tree** uni = (Tree**)malloc(sizeof(Tree*));
-	*uni = makeTreeFromBits(bitTree, &n);
-	int** table = makeEncodingTable(uni);
-	/*for (int i = 0; i < (int)UNIQUE_BYTES; i++) {
-		if (table[i] != NULL) {
-			printf("%c: %d, %d\n", (char)i, table[i][0], table[i][1]);
-		}
-	}*/
-
-
-	/*
-	Tree** uni = initializeUnicodeFrequencies();
-	long long* count = (long long*)malloc((int)UNIQUE_BYTES * sizeof(long long));
-	getUnicodeFrequencies(uni, count, fpi);
-	fseek(fpi, 0, SEEK_SET);
-	sortTreesDescending(uni, (int)UNIQUE_BYTES);
-	makeTreeFromSorted(uni);
-	int** table = makeEncodingTable(uni);
-	*/
-	return;
-}
-
-void printTree(Tree* t) {
-	if (t == NULL){
-		printf("");
-	} else if (t->n != 255) {
-		printf("%c: %d\n", (char)t->n, (int)t->f);
-	} else {
-		printf("%d\n", (int)t->f);
-		printTree(t->left);
-		printTree(t->right);
-		printf("<-%d\n", (int)t->f);
-	}
-	return;
-}
-
-void testTreeSort() {
-	Tree** uni = initializeUnicodeFrequencies();
-	FILE* fp;
-	fopen_s(&fp, "test.txt", "r");
-	long long* count = (long long*)malloc((int)UNIQUE_BYTES * sizeof(long long));
-	getUnicodeFrequencies(uni, count, fp);
-	sortTreesDescending(uni, (int)UNIQUE_BYTES);
-	for (int i = 0; i < (int)UNIQUE_BYTES; i++) {
-		if (uni[i]->f == 0)
-			break;
-		printf("%c: %d\n", (char) uni[i]->n, (int) uni[i]->f);
-	}
-	if (fp != NULL)
-		fclose(fp);
-	return;
-}
-
-void testTreeAssembly() {
-	Tree** uni = initializeUnicodeFrequencies();
-	FILE* fp;
-	fopen_s(&fp, "test.txt", "r");
-	long long* count = (long long*)malloc((int)UNIQUE_BYTES * sizeof(long long));
-	getUnicodeFrequencies(uni, count, fp);
-	if (fp != NULL)
-		fclose(fp);
-	sortTreesDescending(uni, (int)UNIQUE_BYTES);
-	makeTreeFromSorted(uni);
-	printf("%d\n", (int)uni[0]->f);
-	printTree(uni[0]);
-	return;
-}
-
-void testEncodingTable() {
-	Tree** uni = initializeUnicodeFrequencies();
-	FILE* fp;
-	fopen_s(&fp, "test.txt", "r");
-	long long* count = (long long*)malloc((int)UNIQUE_BYTES * sizeof(long long));
-	getUnicodeFrequencies(uni, count, fp);
-	if (fp != NULL)
-		fclose(fp);
-	sortTreesDescending(uni, (int)UNIQUE_BYTES);
-	makeTreeFromSorted(uni);
-	int** table = makeEncodingTable(uni);
-	for (int i = 0; i < (int)UNIQUE_BYTES; i++) {
-		if (table[i] != NULL) {
-			printf("%c: %d, %d\n", (char)i, table[i][0], table[i][1]);
-		}
-	}
-	return;
-}
