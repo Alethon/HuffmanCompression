46f313a8eaad735684c95f6c4a2c3042c695984f
unknown <jacob.laster23@gmail.com>
2020-01-08 15:36:06 -0500

    changing to clearer header structure

--- HuffmanCompression/huff.h
+++ HuffmanCompression/huff.h
@@ -1,22 +1,17 @@
 /*this is huff.h*/
 
+#include "huffTree.h"
+
 #ifndef HUFF
 #define HUFF
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
-
-/* a binary tree with two members
- * n holds the character to be encoded (or -1 if the Tree is a branch)
- * f holds the character's frequency
- */
-typedef struct _Tree {
-	struct _Tree * left;
-	struct _Tree * right;
-	long long f;
-	uint8_t n;
-} Tree;
+Tree** initializeUnicodeFrequencies(void);
+void getUnicodeFrequencies(Tree**, long long*, FILE*);
+int getPaddingBits(long long*, int**);
+void getPreorderFromTree(Tree*, uint8_t*, int*);
+void writeHeader(FILE*, int, int, uint8_t*);
+int huffmanCompress(int**, FILE*, FILE*);
+void huff(char*, char*);
 
 #endif /*HUFF*/
 
